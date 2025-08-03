5f3600b3e452d19f8b09ee963b1cf9b294fd9011
Amanuel <beginningoflife2324@gmail.com>
2025-08-03 01:46:25 -0700

    simple bank account cli

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+   def __init__(self, account_balance=0):
+      self.account_balance = account_balance
+
+   def deposit(self, amount):
+      if amount > 0:
+            self.account_balance += amount
+   def withdraw(self, amount):
+       if amount >0 and amount <= self.account_balance:
+           self.account_balance -= amount
+           return True
+       return False 
+   def display_balance(self):
+        if self.account_balance < 0:
+            print("Account is overdrawn.")
+        else:
+         print(f"Current balance: ${self.account_balance:.2f}")            
