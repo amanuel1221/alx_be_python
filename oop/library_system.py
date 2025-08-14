01dd9722ad754b33eb0ba57d34e6707403d1f9c3
Amanuel <beginningoflife2324@gmail.com>
2025-08-14 01:26:48 -0700

    creating the library system

---
+++ oop/library_system.py
@@ -0,0 +1,43 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        """Base class for all books."""
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        """EBook subclass with an extra file_size attribute."""
+        super().__init__(title, author)  # Call base class constructor
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        """PrintBook subclass with an extra page_count attribute."""
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        """Library stores a collection of books (composition)."""
+        self.books = []
+
+    def add_book(self, book: Book):
+        """Add a Book, EBook, or PrintBook instance to the library."""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of each book in the library."""
+        for book in self.books:
+            print(book)  # Calls the __str__ method of each book
